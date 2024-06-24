7d6d85c8337bc096481955b51425acfc26723b22
jonmc89 <126274870+jonmc89@users.noreply.github.com>
2024-06-24 14:23:25 +0100

    Header and Nav Bar Work Started

---
+++ script.js
@@ -0,0 +1,9 @@
+function sideMenuOpen() {
+  const sideMenuOpen = document.getElementById("sidenav");
+  sideMenuOpen.style.display = "block";
+}
+
+function sideMenuClose() {
+  const sideMenuClose = document.getElementById("sidenav");
+  sideMenuClose.style.display = "none";
+}
